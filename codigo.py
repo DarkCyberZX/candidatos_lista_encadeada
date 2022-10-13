e5d059f0520f57274ab67a1bf009d27c8498a2ea
DarkCyberZX <94545745+DarkCyberZX@users.noreply.github.com>
2022-10-13 20:06:18 -0300

    Update codigo.py

--- codigo.py
+++ codigo.py
@@ -1,90 +1,12 @@
 class Node :
     def __init__(self,data) :
         self.data = data
-        self.next = None
+        self.proximo = None
 
-class Lista:
+class Encadeada:
     def __init__(self) :
         self.head = None
-        self._size = 0
-
-    def lista_vazia(self) :
-        if self._size == 0 :
-            return True
-        else :
-            return False
-
-    def tamanho(self) :
-        if lista.lista_vazia() == False:
-            return self._size
-
-        return "fila Vazia"
-
-    def adicionar(self ,candidato):
-        node = Node (candidato)
-        if self.head == None:
-            self.head = node
-
-        else:
-            ponteiro = self.head
-            while ponteiro.next != None :
-                ponteiro = ponteiro.next
-            ponteiro.next = node
-        self._size += 1
-
-    def remove(self):
-        if self._size > 0:
-            print ("Candidato removido com sucesso. \n")
-            self.head = self.head.next
-
-        else :
-            print ('fila vazia')
-
-        if self.head is None :
-            self.head = None
-
-        self._size -= 1
-
-    def primeiro_candidato(self) :
-        if lista.lista_vazia() == True:
-            return("Fila Vazia.")
-
-        return str(f"{self.head.data}\n")
-
-    def rodar_lista(self):
-        node = Node(str(self.head.data))
-        if (self.head == None) :
-            self.head = node
-        else :
-            ponteiro = self.head
-            while ponteiro.next != None:
-                ponteiro = ponteiro.next
-            ponteiro.next = node
-
-        if self._size > 0 :
-            self.head = self.head.next
-
-        else :
-            print ('fila vazia')
-
-        if self.head is None :
-            self.head = None
-
-    def __repr__(self) :
-        if self.lista_vazia():
-            return 'Fila vazia'
-        candidatos = ''
-        numero_de_candidatos = self.head
-        while(numero_de_candidatos):
-            candidatos +=str(numero_de_candidatos.data).capitalize()
-            numero_de_candidatos = numero_de_candidatos.next
-            if (numero_de_candidatos):
-                candidatos += ' '
-        return f"{candidatos} \n"
-
-    def rodar_lista_n_vezes(self,num):
-        for x in range(num):
-            lista.rodar_lista()
+        self.tamanho = 0
 
     def menu(self):
         resposta = str(input("1- Adicionar candidato\n"
@@ -94,32 +16,98 @@ class Lista:
               "5- Girar a fila dos candidatos\n"
               "6- Girar a fila dos candidatos X vezes\n"))
 
-        lista.opcoes(resposta)
+        candidatos_encadeada.opcoes(resposta)
 
     def opcoes(self,resposta):
         if resposta == "1":
             candidato = str(input("Digite o nome do Candidato: ")).capitalize()
-            lista.adicionar(candidato)
-            print("Candidato adicionado com sucesso.\n")
+            candidatos_encadeada.adicionar_candidato(candidato)
 
         elif resposta == "2":
-            print(lista.primeiro_candidato())
+            print(candidatos_encadeada.primeiro_candidato())
 
         elif resposta == "3":
-            lista.remove()
+            print(candidatos_encadeada.remover_candidato())
 
         elif resposta == "4":
-            print(lista)
+            print(candidatos_encadeada.todos_candidatos())
 
         elif resposta == "5":
-            lista.rodar_lista()
-            print("Lista girada com sucesso.\n")
+            candidatos_encadeada.rodar_lista()
+            if len(candidatos_encadeada) > 0:
+                print("Lista girada com sucesso.\n")
 
         elif resposta == "6":
-            resposta = int(input("Digite quantas vezes a fila deve ser girada: "))
-            lista.rodar_lista_n_vezes(resposta)
-            print(f"A lista foi girada {resposta} vezes com sucesso.\n")
+            if len(candidatos_encadeada) == 0:
+                print("Lista Vazia. \n")
 
-lista = Lista()
+            else:
+                resposta = int(input("Digite quantas vezes a fila deve ser girada: "))
+                candidatos_encadeada.rodar_lista(resposta)
+                print(f"A lista foi girada {resposta} vezes com sucesso.\n")
+
+
+    def __len__(self):
+        return self.tamanho
+
+    def adicionar_candidato(self,candidato):
+        node = Node(candidato)
+        if self.head == None:
+            self.head = node
+
+        else:
+            ponteiro = self.head
+            while ponteiro.proximo != None :
+                ponteiro = ponteiro.proximo
+            ponteiro.proximo = node
+        self.tamanho += 1
+        print("Candidato adicionado com sucesso.\n")
+
+    def remover_candidato(self):
+        if len(candidatos_encadeada) > 0:
+            self.head = self.head.proximo
+            self.tamanho -= 1
+        else:
+            return("fila Vazia. \n")
+
+
+        if self.head is None:
+            self.head = None
+
+        return ("Candidato removido com sucesso. \n")
+
+
+    def primeiro_candidato(self) :
+        if len(candidatos_encadeada) == 0:
+            return("Fila Vazia. \n")
+
+        return str(f"{self.head.data}\n")
+
+    def rodar_lista(self,numero_vezes = 1):
+        if len(candidatos_encadeada) == 0:
+            print("Lista Vazia. \n")
+
+        else:
+            for x in range(numero_vezes):
+                node = Node(str(self.head.data))
+                ponteiro = self.head
+                while ponteiro.proximo != None:
+                    ponteiro = ponteiro.proximo
+                ponteiro.proximo = node
+                self.head = self.head.proximo
+
+    def todos_candidatos(self):
+        if len(candidatos_encadeada) == 0:
+            return 'Fila Vazia. \n'
+        candidatos = ''
+        numero_de_candidatos = self.head
+        while(numero_de_candidatos):
+            candidatos +=str(numero_de_candidatos.data).capitalize()
+            numero_de_candidatos = numero_de_candidatos.proximo
+            if (numero_de_candidatos):
+                candidatos += ' '
+        return f"{candidatos} \n"
+
+candidatos_encadeada = Encadeada()
 while (True):
-    lista.menu()
+    candidatos_encadeada.menu()
